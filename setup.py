e85ec71a30a058ec2be82d36032a05cd7f246bae
jade5638 <jade.nassif2002@gmail.com>
2024-06-09 15:51:45 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Hypersonic Waverider Generator'
+LONG_DESCRIPTION = 'Package which allows a user to generate and export parametric waverider geometries'
+
+setup(
+    name="waverider_generator",
+    version=VERSION,
+    author="Jade Nassif",
+    author_email="<jade.nassif2002@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy','cadquery','scipy','matplotlib'],
+    keywords=['python', 'waverider', 'CAD', 'hypersonic','aerospace-engineering']
+)
