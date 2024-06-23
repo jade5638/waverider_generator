7171ee9d3fece009c63119f1d2b7ec7de38ef322
Jade Nassif <jade.nassif2002@gmail.com>
2024-06-23 15:28:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.1.3'
+VERSION = '2.0.0'
 DESCRIPTION = 'Hypersonic Waverider Generator'
 LONG_DESCRIPTION = 'Package which allows a user to generate and export parametric waverider geometries'
 
